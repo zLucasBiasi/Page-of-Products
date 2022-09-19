7c28bd617e83e42a1ad54711604b904cbc85c3ef
zLucasBiasi <lucasbiasi2001@gmail.com>
2022-09-19 20:29:52 -0300

    fazendo alguns ajustes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import { Loading } from "../../components/Loading";
 import { getApiData } from "../../services/api";
--- src/pages/Produto/index.jsx
+++ src/pages/Produto/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-
 import { useParams } from "react-router-dom";
+
 import { Loading } from "../../components/Loading";
 import { getApiData } from "../../services/api";
 
